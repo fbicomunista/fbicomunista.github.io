1de49e0202bfc61ac5163f959fc9efd418915b2a
Kimi Djan Paes Teixeira <71793652+fbicomunista@users.noreply.github.com>
2022-09-17 20:34:48 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,16 @@
+
+function darkMode(){
+
+    document.getElementById("body-darkMode").style.backgroundColor = "black"
+	document.getElementById("body-darkMode").style.color = "white"
+	var input = document.getElementById("dark-mode") 
+	input.setAttribute("onclick", "lightMode()")
+}
+
+function lightMode(){
+
+	document.getElementById("body-darkMode").style.backgroundColor = "white"
+	document.getElementById('body-darkMode').style.color = "Black"
+	var input = document.getElementById("dark-mode")
+	input.setAttribute("onclick", "darkMode()")
+}
